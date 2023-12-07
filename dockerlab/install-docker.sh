60926a4e744162cb18accec83c2b10c3517a0ca3
Bert Van Vreckem <bert.vanvreckem@hogent.be>
2023-12-07 09:52:02 +0100

    Fix bug in Docker install script

--- dockerlab/install-docker.sh
+++ dockerlab/install-docker.sh
@@ -87,7 +87,7 @@ if docker ps | grep -q portainer; then
     log "Portainer already running"
 else
     log "Starting Portainer"
-    docker run --detached \
+    docker run --detach \
         --publish 8000:8000 --publish 9000:9000 \
         --name=portainer --restart=always \
         --volume /var/run/docker.sock:/var/run/docker.sock \
